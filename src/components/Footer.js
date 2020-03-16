2c3692493cdcd3d99f36c47163695321acb58eaa
saurabhgavali23 <gavalisaurabh02@gmail.com>
2020-03-16 12:46:06 +0530

    Added: Functionality to Display Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../css/Footer.css'
+
+export default function footer() {
+  return (
+    <div className="footer">
+      <p>Copyright &copy; 2020, Bookstore Private Ltd. All Rights Reserved</p>
+    </div>
+  );
+}
