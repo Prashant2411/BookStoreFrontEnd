f9343ec6b8dbcf962b70eb9490d6da9135f9a722
AmarArjunGunjal <amargunjal28@gmail.com>
2020-03-17 12:15:17 +0530

    Added : AppBar and validation

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
@@ -6,12 +6,34 @@ import TextareaAutosize from "@material-ui/core/TextareaAutosize";
 import { withStyles } from "@material-ui/core/styles";
 import "../App.css";
 import {addBookConfiguration,addUploadConfiguration} from '../configuration/configuration'
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import MenuBookIcon from "@material-ui/icons/MenuBookSharp";
 
 const useStyles = theme => ({
+  grow: {
+    flexGrow: 1,
+  },
+  bookIcon: {
+    padding: "0 0 0 2%",
+    fontSize: "36px",
+    [theme.breakpoints.up("sm")]: {
+      padding: "0 0 0 10%",
+      fontSize: "36px"
+    }
+  },
+  title: {
+    display: "none",
+    paddingLeft: "0.5%",
+    fontSize: "140%",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
+  },
   root: {
-    border: "3px solid rgb(24, 12, 196)",
+    border: "3px solid rgb(145,10,10)",
     borderRadius: "10px",
-    margin: "7% 15% 7% 15%",
+    margin: "2% 15% 3% 15%",
     padding: "2% 5% 2% 20%",
     "& > *": {
       margin: theme.spacing(1),
@@ -20,16 +42,17 @@ const useStyles = theme => ({
   },
   textArea: {
     width: "69%",
-    borderRadius: "5px"
+    borderRadius: "5px",
+    resize: "none"
   },
   addBook: {
     marginLeft: "11%",
-    backgroundColor: "rgb(24, 12, 196)",
+    backgroundColor: "rgb(145,10,10)",
     width: "50%",
     color: "white"
   },
   heading: {
-    color: "rgb(24, 12, 196)",
+    color: "rgb(145,10,10)",
     width: "100%",
     marginTop: "1%"
   },
@@ -43,9 +66,9 @@ class BasicTextFields extends Component {
   state = {
     Title: "",
     Author: "",
-    Price: 0,
-    Stock: 0,
-    Year: 0,
+    Price: "",
+    Stock: "",
+    Year: "",
     BookDetails: "",
     files: "",
     imgName: ""
@@ -55,12 +78,41 @@ class BasicTextFields extends Component {
     this.setState({ files: event.target.value });
   };
 
-  updateState = (event) => {
+  updateState = (event) => {    
     this.setState({ [event.target.name]: event.target.value })
   }
 
   addBook = () => {
     addBookConfiguration(this.state);
+  }
+  
+  validateAuthor = (event) => {
+    const regexp = /^[A-Za-z]{3,20}$/;
+    const char=event.target.value;
+    if(!regexp.test(char)){
+      alert("Invalid Author Name")
+      this.setState({
+        [event.target.name]:""
+      })
+    }
+  }
+
+  validateYear = async (event) => {
+    const yearVal = event.target.value;
+    const regexp = /[1-2]\d{3}$/
+    if(!regexp.test(yearVal)){
+      alert("Invalid Year")
+      await this.setState({
+        [event.target.name]:""
+      })
+    }
+    else if(yearVal > 2020)
+    {
+      alert("Year should be in range 1100 to current year");
+      await this.setState({
+        [event.target.name]:""
+      })
+    }
   }
 
   uploadButtonClick = async (event) => {
@@ -80,7 +132,18 @@ class BasicTextFields extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <form className={classes.root} noValidate autoComplete="off">
+      <Fragment>
+      <div className={classes.grow}>
+      <AppBar position="static" style={{backgroundColor: "rgb(145,10,10)"}}>
+        <Toolbar>
+          <MenuBookIcon className={classes.bookIcon} />
+          <Typography className={classes.title} variant="h6" noWrap>
+            BookStore
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+      <form onSubmit={this.addBook} className={classes.root} autoComplete="off">
         <Typography
           className={classes.heading}
           variant="h4"
@@ -94,13 +157,17 @@ class BasicTextFields extends Component {
           variant="outlined"
           name="Title"
           onChange={this.updateState}
+          required
         />
         <br />
         <TextField
           label="Author"
           variant="outlined"
           name="Author"
+          value={this.state.Author}
+          onBlur={this.validateAuthor}
           onChange={this.updateState}
+          required
         />
         <br />
         <TextField
@@ -109,14 +176,18 @@ class BasicTextFields extends Component {
           variant="outlined"
           name="Price"
           onChange={this.updateState}
+          required
         />
         <br />
         <TextField
-          type="number"
+          type="text"
           label="Year"
           variant="outlined"
           name="Year"
+          value={this.state.Year}
+          onBlur={this.validateYear}
           onChange={this.updateState}
+          required
         />
         <br/>
         <TextField
@@ -124,7 +195,9 @@ class BasicTextFields extends Component {
           label="Stock"
           variant="outlined"
           name="Stock"
+          value={this.state.Stock}
           onChange={this.updateState}
+          required
         />
         <br />
         <TextareaAutosize
@@ -134,6 +207,7 @@ class BasicTextFields extends Component {
           placeholder="Book Detail"
           name="BookDetails"
           onChange={this.updateState}
+          required
         />
         <br />
         <Button variant="contained" component="label">
@@ -144,18 +218,19 @@ class BasicTextFields extends Component {
             name="image"
             onChange={this.uploadButtonClick}
             accept="Image/*"
+            required
           />
         </Button>
         <p className={classes.url}>{this.state.imgName}</p>
         <Button
+          type="submit"
           className={classes.addBook}
           variant="contained"
-          component="label"
-          onClick={this.addBook}
         >
           Add Book
         </Button>
       </form>
+      </Fragment>
     );
   }
 }
